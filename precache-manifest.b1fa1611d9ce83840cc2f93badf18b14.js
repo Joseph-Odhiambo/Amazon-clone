74abb19de33c233fcaca57687cd8f93ce8c0fea2
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-27 19:06:42 +0300

    Updates

---
+++ precache-manifest.b1fa1611d9ce83840cc2f93badf18b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622cad44abe0a101b77b83194b334d61",
+    "url": "/http//Joseph-Odhiambo.github.io/Amazone-clone/index.html"
+  },
+  {
+    "revision": "cc41ef762444d96f8908",
+    "url": "/http//Joseph-Odhiambo.github.io/Amazone-clone/static/css/main.ae50d68e.chunk.css"
+  },
+  {
+    "revision": "0ae85110042d8818e006",
+    "url": "/http//Joseph-Odhiambo.github.io/Amazone-clone/static/js/2.0005cbaa.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/http//Joseph-Odhiambo.github.io/Amazone-clone/static/js/2.0005cbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc41ef762444d96f8908",
+    "url": "/http//Joseph-Odhiambo.github.io/Amazone-clone/static/js/main.e3a65b74.chunk.js"
+  },
+  {
+    "revision": "153a68b148335c039048",
+    "url": "/http//Joseph-Odhiambo.github.io/Amazone-clone/static/js/runtime-main.d6a3f3ae.js"
+  }
+]);
